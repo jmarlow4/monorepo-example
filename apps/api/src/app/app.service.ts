155a9a0b0649527b332d5d5a76e4dc3781cc1778
Jason Marlow <jasonmarlow@gmail.com>
2022-04-18 16:46:04 -0600

    ci: testing nx builds

--- apps/api/src/app/app.service.ts
+++ apps/api/src/app/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getData(): { message: string } {
-    return { message: 'Hello from API!' };
+    return { message: 'Hello from API!!!! (with NX)' };
   }
 }
