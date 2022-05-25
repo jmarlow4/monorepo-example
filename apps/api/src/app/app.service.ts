498c2477c49ddcc3430ef81ce829eaa3f335a971
Jason Marlow <jasonmarlow@gmail.com>
2022-05-24 23:26:55 -0600

    build: testing build one last time

--- apps/api/src/app/app.service.ts
+++ apps/api/src/app/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getData(): { message: string } {
-    return { message: 'Hello from API!!!! (With NX Cloud)' };
+    return { message: 'Hello from API!' };
   }
 }
